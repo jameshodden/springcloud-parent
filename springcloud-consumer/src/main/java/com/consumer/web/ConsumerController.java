3410e1a777248898364b60ba5ef631834997475b
xm <xm@qq.com>
2018-10-23 21:31:17 +0800

    消费者加注释

--- springcloud-consumer/src/main/java/com/consumer/web/ConsumerController.java
+++ springcloud-consumer/src/main/java/com/consumer/web/ConsumerController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 消费者示例
+ */
 @RestController
 public class ConsumerController {
 
